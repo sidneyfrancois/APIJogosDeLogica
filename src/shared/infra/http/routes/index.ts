7789d812bcf102b950f64412039b10ac541c46a2
Sidney François <sidney.fvm.ps@outlook.com>
2022-08-11 09:34:55 -0300

    Inserindo rota de autenticação

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { authenticateRoutes } from "./authenticate.routes";
 import { usersRoutes } from "./user.routes";
 
 const router = Router();
 
 router.use("/user", usersRoutes);
+router.use("/authenticate", authenticateRoutes);
 
 export { router };
