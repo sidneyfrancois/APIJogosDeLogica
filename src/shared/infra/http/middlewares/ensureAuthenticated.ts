d022d6478e4ecb0202ada2bd04e664d36ef58a55
Sidney François <sidney.fvm.ps@outlook.com>
2022-08-11 09:38:17 -0300

    criando middleware para verificação de autenticação de usuário

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UsersRepository } from "../../../../modules/accounts/infra/repositories/UserRepository";
+import { AppError } from "../../../errors/AppError";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "1669198dbf6ea910ecce0172e14e7e52"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exists!", 401);
+    }
+
+    request.user = {
+      id: user_id,
+    };
+    next();
+  } catch {
+    throw new AppError("Invalid token!", 401);
+  }
+}
