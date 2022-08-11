c8b016372c0ddfa419a72edb2a41f4e6f815362b
Sidney François <sidney.fvm.ps@outlook.com>
2022-08-11 09:21:43 -0300

    Criando migration de usuário

---
+++ src/shared/infra/database/migrations/1660220298410-CreateUser.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUser1660220298410 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "escola",
+            type: "varchar",
+          },
+          {
+            name: "isInscrutor",
+            type: "boolean",
+            default: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
